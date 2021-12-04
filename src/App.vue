49cbb9ec5ce667d19543fd5194cb4d49e553f3b2
Lyden Chai <lyden.chai@PNCLB12-23.pnc.lan>
2021-12-04 10:31:01 +0700

    Bootstrap Vue Modal

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <navbar></navbar>
+    <router-view/>
+</template>
+
+<script>
+import Navbar from './components/navbar/Navbar.vue';
+export default {
+  components: {
+    'navbar': Navbar,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
